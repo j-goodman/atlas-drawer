9ef0617679d3af422b6aef6f31c6959422f1ccd5
John Goodman <johncurtisgoodman@gmail.com>
2017-04-30 20:22:00 -0400

    Add dynamic scale and offset changing

--- script.js
+++ script.js
@@ -5,30 +5,69 @@ var offset = {
 
 var scale = 0.7;
 
-onload = function () {
+window.onload = function () {
   var i; var j;
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
+  var scaleLabel = document.getElementById('scale-label');
+  var offsetx = document.getElementById('x-label');
+  var offsety = document.getElementById('y-label');
+
+  scaleLabel.innerText = 'scale';
+  offsetx.innerText = 'offset.x';
+  offsety.innerText = 'offset.y';
+
+  loadMap(ctx, map);
+};
+
+var loadMap = function (ctx, map) {
+  ctx.clearRect(0, 0, 1280, 960);
+  console.log('Loading map...');
   for (i=0 ; i<map.allNames.length ; i++) {
     var coord;
-    console.log(map.allNames[i]);
     ctx.beginPath();
     ctx.strokeStyle = '#fff';
 
-    coord = map[map.allNames[i]][0];
+    // if (map.allNames[i] === 'Coahuila') {
+    //   ctx.strokeStyle = '#00f';
+    // } else {
+    //   ctx.strokeStyle = '#fff';
+    // }
+
+    if (map[map.allNames[i]]) {
+      coord = map[map.allNames[i]][0];
+    } else {
+      continue;
+    }
     ctx.moveTo((coord.x - offset.x) * scale, (coord.y - offset.y) * scale);
-    console.log((coord.x - offset.x) * scale, (coord.y - offset.y) * scale);
 
     for (j=1 ; j<map[map.allNames[i]].length ; j++) {
       coord = map[map.allNames[i]][j];
       ctx.lineTo((coord.x - offset.x) * scale, (coord.y - offset.y) * scale);
       ctx.stroke();
-      console.log((coord.x - offset.x) * scale, (coord.y - offset.y) * scale);
     }
 
     coord = map[map.allNames[i]][0];
     ctx.lineTo((coord.x - offset.x) * scale, (coord.y - offset.y) * scale);
     ctx.stroke();
-    console.log((coord.x - offset.x) * scale, (coord.y - offset.y) * scale);
   }
+  console.log('Loaded.');
+};
+
+window.onkeydown = function () {
+  setTimeout(function () {
+    var scaleMeter = document.getElementById('scale');
+    var offsetx = document.getElementById('offset-x');
+    var offsety = document.getElementById('offset-y');
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    offset = {
+      x: offsetx.value,
+      y: offsety.value,
+    };
+    scale = scaleMeter.value;
+
+    loadMap(ctx, map);
+  }, 1000);
 };
